3f6e0ad049158b705d43e35a183a20652eba0866
barslmn <barslmn@gmail.com>
2020-02-18 09:44:16 +0300

    commit

--- django_chatter/context_processors.py
+++ django_chatter/context_processors.py
@@ -10,14 +10,20 @@ def get_chatroom_list(request):
             rooms_with_unread = []
             # Go through each list of rooms and check if any of the latest 50 messages
             # was unread
+            unread_count = 0
             for room in rooms_list:
                 for message in room.message_set.all().order_by('-id')[:50]:
                     if request.user not in message.recipients.all():
                         rooms_with_unread.append(room.id)
+                        unread_count += 1
                         break
 
             # unread rooms are marked with bold letterings
-            return ({'rooms_list': rooms_list, 'rooms_with_unread': rooms_with_unread})
+            return ({
+                    'rooms_list': rooms_list,
+                    'rooms_with_unread': rooms_with_unread,
+                    'unread_count': unread_count
+                    })
         else:
             return ({})
     else:
